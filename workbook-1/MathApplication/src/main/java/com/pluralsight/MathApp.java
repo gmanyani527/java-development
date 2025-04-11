7d349055da0ca068a8093f3973249a3b15475da3
gmanyani527 <131324621+gmanyani527@users.noreply.github.com>
2025-04-11 16:16:26 -0400

    Upload Solutions for Week1

---
+++ workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+        int bobSalary = 10;
+        int garySalary = 20;
+
+        double highestSalary = Math.max(bobSalary, garySalary);
+
+
+        System.out.println(highestSalary);
+    }
+}
